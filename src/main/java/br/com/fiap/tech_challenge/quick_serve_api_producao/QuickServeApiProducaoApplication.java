6ad6a012d66bd08609e02aaf15cdfe19574f1cad
Sergio Artero <sergio.staff@gmail.com>
2024-11-21 14:56:45 -0300

    setup

---
+++ src/main/java/br/com/fiap/tech_challenge/quick_serve_api_producao/QuickServeApiProducaoApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.tech_challenge.quick_serve_api_producao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickServeApiProducaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuickServeApiProducaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/tech_challenge/quick_serve_api_producao/QuickServeApiProducaoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.tech_challenge.quick_serve_api_producao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickServeApiProducaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
