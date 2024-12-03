529698e5804f1b67f1a79e0b1c70b8159fc77878
Lorraine Paloschi <lorraine.paloschi@gmail.com>
2024-12-03 14:08:50 -0300

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -1,67 +1,6 @@
-provider "aws" {
-  region = "us-east-1"
-}
+# Obtendo a identidade da conta atual
+data "aws_caller_identity" "current" {}
 
-# Security group
-resource "aws_security_group" "security_group" {
-  name        = "security_group"
-  description = "Permitir o acesso http e acesso à Internet"
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  ingress {
-    from_port   = 8080
-    to_port     = 8083
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  ingress {
-    from_port   = 27017
-    to_port     = 27017
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  egress {
-    from_port   = 0
-    to_port     = 65535
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-# VPC
-resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
-}
-
-# Subnet for Fargate
-resource "aws_subnet" "subnet" {
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.0.1.0/24"
-  availability_zone       = "us-east-1a"
-  map_public_ip_on_launch = true
-}
-
-# ECS Cluster
-resource "aws_ecs_cluster" "ecs_cluster" {
-  name = "prod"
-}
-
-# ECR repository
-resource "aws_ecr_repository" "ecr_repo" {
-  name = "quick-serve-api-producao"
-}
-
-# ECS Task Definition for MongoDB, App, and Mongo-Express containers
 resource "aws_ecs_task_definition" "app_task" {
   family                   = "quick-serve-api-producao"
   execution_role_arn       = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/ecsTaskExecutionRole"
@@ -112,47 +51,4 @@ resource "aws_ecs_task_definition" "app_task" {
       ]
     }
   ])
-}
-
-# ECS Service for Fargate
-resource "aws_ecs_service" "ecs_service" {
-  name            = "api-producao"
-  cluster         = aws_ecs_cluster.ecs_cluster.id
-  task_definition = aws_ecs_task_definition.app_task.arn
-  desired_count   = 1
-  launch_type     = "FARGATE"
-  network_configuration {
-    subnets          = [aws_subnet.subnet.id]
-    security_groups = [aws_security_group.security_group.id]
-    assign_public_ip = true
-  }
-}
-
-# IAM Role for ECS task execution
-resource "aws_iam_role" "ecs_task_execution_role" {
-  name = "ecsTaskExecutionRole"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Principal = {
-          Service = "ecs-tasks.amazonaws.com"
-        }
-        Effect = "Allow"
-        Sid    = ""
-      }
-    ]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "ecs_task_execution_policy" {
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
-  role       = aws_iam_role.ecs_task_execution_role.name
-}
-
-# Outputs
-output "ecr_repository_url" {
-  value = aws_ecr_repository.ecr_repo.repository_url
 }
