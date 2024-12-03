95ca8d09eb1f364be34c659691114ee8e5d24ceb
Lorraine Paloschi <lorraine.paloschi@gmail.com>
2024-12-03 14:49:04 -0300

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -1,53 +1,41 @@
 # Obtendo a identidade da conta atual
 data "aws_caller_identity" "current" {}
 
-# Declarando o repositório ECR
-resource "aws_ecr_repository" "ecr_repo" {
-  name                 = "quick-serve-api-producao"
-  image_tag_mutability = "MUTABLE"
-  lifecycle {
-    ignore_changes = [image_tag_mutability]
-  }
-}
+resource "aws_ecs_task_definition" "app_task" {
+  family                   = "quick-serve-api-producao"
+  execution_role_arn       = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/ecsTaskExecutionRole"
+  task_role_arn            = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/ecsTaskExecutionRole"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = "256"
+  memory                   = "512"
 
-# Definição da Task no ECS
-    resource "aws_ecs_task_definition" "app_task" {
-      family                = "quick-serve-api-producao"
-      requires_compatibilities = ["FARGATE"]
-      network_mode          = "awsvpc"
-      cpu                   = "512"  # Aumente esse valor para acomodar todos os containers
-      memory                = "1024"  # Ajuste a memória conforme necessário
-      execution_role_arn    = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/ecsTaskExecutionRole"
-      task_role_arn         = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/ecsTaskExecutionRole"
-
-      container_definitions = jsonencode([
+  container_definitions = jsonencode([
+    {
+      name      = "mongodb"
+      image     = "mongo:latest"
+      cpu       = 128
+      memory    = 256
+      essential = true
+      portMappings = [
         {
-          name      = "app-container"
-          image     = "your-ecr-repo-uri:latest"
-          cpu       = 256  # CPU do container
-          memory    = 512  # Memória do container
-          essential = true
-          portMappings = [
-            {
-              containerPort = 8080
-              hostPort      = 8080
-            }
-          ]
-        },
-        {
-          name      = "db-container"
-          image     = "mongo:latest"
-          cpu       = 256  # CPU do container
-          memory    = 512  # Memória do container
-          essential = true
-          portMappings = [
-            {
-              containerPort = 27017
-              hostPort      = 27017
-            }
-          ]
+          containerPort = 27017
+          hostPort      = 27017
         }
-      ])
+      ]
+    },
+    {
+      name      = "app"
+      image     = "${aws_ecr_repository.ecr_repo.repository_url}:latest"
+      cpu       = 128
+      memory    = 256
+      essential = true
+      portMappings = [
+        {
+          containerPort = 8080
+          hostPort      = 8080
+        }
+      ]
     },
     {
       name      = "mongo-express"
